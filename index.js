6145a7a21ff8b670e807ebf220eb0dd3cdc0fe3d
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2022-04-25 23:06:30 +0600

    deploying heroku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5003;
@@ -8,6 +9,22 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+function verifyJwt(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthirazied access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    console.log("decoded", decoded);
+    req.decoded = decoded;
+    next();
+  });
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wmied.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -20,6 +37,21 @@ async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db("geniusCar").collection("service");
+    const orderCollection = client.db("geniusCar").collection("service");
+
+    //Auth
+    app.get("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(
+        (user,
+        process.env.ACCESS_TOKEN,
+        {
+          expiresIn: "1d",
+        })
+      );
+      res.send({ accessToken });
+    });
+
     app.get("/service", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
@@ -38,6 +70,36 @@ async function run() {
     app.post("/service", async (req, res) => {
       const newService = req.body;
       const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
+
+    //DELETE
+
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //order collection api
+
+    app.get("/order", verifyJwt, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      } else {
+        res.status(403).send({ message: "forbidden access" });
+      }
+    });
+
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
       res.send(result);
     });
   } finally {
